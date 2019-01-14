edd97042cdbecaa181cb4f69665c0ace4a21a61e
sabelogumede <iamsabelogumede@gmail.com>
2019-01-14 15:33:36 +0200

    userroutes.js

---
+++ models/users.model.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose'),
+Schema = mongoose.Schema;
+
+let UserSchema = new Schema({
+    username: {
+        type: string,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    isOver21: {
+        type: Boolean,
+        required: false
+    }
+});
+
+
+module.exports = mongoose.model('User', UserSchema);
